a774c979c76837b2f336570b381a407f3e0e1970
whizvox <corneiliouseanes@gmail.com>
2026-02-03 15:11:25 -0800

    Add functionality to "Find similar images" button in reserves panel
    Signed-off-by: whizvox <corneiliouseanes@gmail.com>

--- src/main/java/me/whizvox/dailyimageposter/gui/reserve/EditReservePanel.java
+++ src/main/java/me/whizvox/dailyimageposter/gui/reserve/EditReservePanel.java
@@ -1,7 +1,8 @@
 package me.whizvox.dailyimageposter.gui.reserve;
 
 import me.whizvox.dailyimageposter.DailyImagePoster;
-import me.whizvox.dailyimageposter.post.Post;
+import me.whizvox.dailyimageposter.gui.post.SimilarImagesPanel;
+import me.whizvox.dailyimageposter.image.ImageManager;
 import me.whizvox.dailyimageposter.reserve.Reserve;
 import me.whizvox.dailyimageposter.util.IOHelper;
 import me.whizvox.dailyimageposter.util.StringHelper;
@@ -147,7 +148,7 @@ public class EditReservePanel extends JPanel {
     );
     setLayout(layout);
 
-    selectImageButton.addActionListener(e -> {
+    selectImageButton.addActionListener(event -> {
       if (lastSelectedDir == null) {
         lastSelectedDir = Paths.get(Objects.requireNonNullElse(DailyImagePoster.getInstance().preferences.getString(DailyImagePoster.PREF_LAST_IMAGE_DIRECTORY), "."));
       }
@@ -159,9 +160,28 @@ public class EditReservePanel extends JPanel {
         try (InputStream in = Files.newInputStream(selectedImageFile)) {
           BufferedImage bufImg = ImageIO.read(in);
           UIHelper.updateImageLabel(imageLabel, bufImg, IMAGE_SIZE);
-        } catch (IOException ex) {
-          DailyImagePoster.LOG.error("Could not load image {}", selectedImageFile, ex);
+        } catch (IOException e) {
+          DailyImagePoster.LOG.error("Could not load image {}", selectedImageFile, e);
         }
+      }
+    });
+    findSimilarButton.addActionListener(event -> {
+      if (selectedImageFile != null) {
+        findSimilarButton.setEnabled(false);
+        SwingUtilities.invokeLater(() -> {
+          List<ImageManager.SimilarImage> images = DailyImagePoster.getInstance().images().findSimilarImages(selectedImageFile, DailyImagePoster.getInstance().preferences.getDouble(DailyImagePoster.PREF_SIMILARITY_THRESHOLD));
+          if (images.isEmpty()) {
+            JOptionPane.showMessageDialog(parent, "No similar images found.");
+          } else {
+            JDialog dialog = new JDialog(parent, "Similar images found");
+            dialog.setContentPane(new SimilarImagesPanel(images, () -> {
+              dialog.pack();
+              dialog.setLocationRelativeTo(parent);
+            }));
+            dialog.setVisible(true);
+          }
+          findSimilarButton.setEnabled(true);
+        });
       }
     });
     saveButton.addActionListener(event -> {
@@ -169,6 +189,21 @@ public class EditReservePanel extends JPanel {
         if (selectedImageFile == null) {
           JOptionPane.showMessageDialog(parent, "Must select an image to create a reserve.", "Warning", JOptionPane.WARNING_MESSAGE);
           return;
+        }
+        List<ImageManager.SimilarImage> images = DailyImagePoster.getInstance().images().findSimilarImages(selectedImageFile, DailyImagePoster.getInstance().preferences.getDouble(DailyImagePoster.PREF_SIMILARITY_THRESHOLD));
+        if (!images.isEmpty()) {
+          int answer = JOptionPane.showConfirmDialog(parent, "Found " + images.size() + " similar image(s). Would you like to reserve this image anyways?", "Question", JOptionPane.YES_NO_OPTION);
+          if (answer == JOptionPane.NO_OPTION) {
+            JDialog dialog = new JDialog(parent, "Similar images found");
+            dialog.setContentPane(new SimilarImagesPanel(images, () -> {
+              dialog.pack();
+              dialog.setLocationRelativeTo(parent);
+            }));
+            dialog.setVisible(true);
+            return;
+          } else if (answer != JOptionPane.YES_OPTION) {
+            return;
+          }
         }
         String fileName;
         try {
